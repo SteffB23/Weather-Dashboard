259a6fb4b7912d3ee1667bb1457b2b2d7e0be9e8
SteffB23 <sbrookens@hotmail.com>
2021-10-17 13:30:35 -0400

    Adding Ajax call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-console.log("GOOD TO GO!");
-
 //Declare a variable to store the searched city
 var city ="";
-// variable declaration
-var searchCity = $("#search-city");
+// 🔥👇👇 variable declaration **NOT DEFINED YET 👇👇🔥**
+var searchCity =$("#search-city");
 var searchButton = $("#search-button");
 var clearButton = $("#clear-history");
 var currentCity = $("#current-city");
@@ -12,6 +10,7 @@ var currentHumidty= $("#humidity");
 var currentWSpeed=$("#wind-speed");
 var currentUvindex= $("#uv-index");
 var sCity=[];
+// 🔥☝️👆 variable declaration **NOT DEFINED YET ☝️👆🔥**
 
 // searches the city to see if it exists in the entries from the storage
 function find(c){
@@ -25,3 +24,25 @@ function find(c){
 
 // API key
 var APIKey = "a4043d7599994e37cdd0d112ce66f9f8";
+
+// Will display the 5 day forecast in current city
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+// AJAX call
+function currentWeather(city){
+    // Here we build the URL so we can get a data from server side.
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+        //Parse the response to display the current weather including the City name, date and the weather icon. 
+        console.log(response);
+    })
+}
